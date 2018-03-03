3d59caba420e95502f99f43c4757c38aefd1de7e
Leeland <leeland_clenista@yahoo.com>
2018-03-03 09:26:31 -0800

    changed server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./controllers/burgers_controller.js")(app);
 
 // Starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
